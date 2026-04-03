bdac39230847a328082d9005c6b328ee72cd2b11
tadsavage <77761466+tadsavage@users.noreply.github.com>
2026-04-03 16:18:44 -0500

    Update Build PhaseChecklistWindow.cs

--- Assets/10. Editor/Build PhaseChecklistWindow.cs
+++ Assets/10. Editor/Build PhaseChecklistWindow.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+//
 public class BuildPhaseChecklistWindow : EditorWindow
 {
     private ChecklistRoot _root;
