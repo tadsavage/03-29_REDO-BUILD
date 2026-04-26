07a32ca69965019cf36aa9b5e7d045bbf084520b
tadsavage <77761466+tadsavage@users.noreply.github.com>
2026-04-26 18:26:53 -0500

    Revert "working on fixing all the UI stuff"
    This reverts commit 8e1c3b55b027cb4b6e61e0a39d301b7914c9c5f0.

--- Assets/3. UI/TopHUD/PreviewCostUI.cs
+++ Assets/3. UI/TopHUD/PreviewCostUI.cs
@@ -17,13 +17,12 @@ public class PreviewCostUI : MonoBehaviour
             return;
 
         _label.style.display = DisplayStyle.Flex;
-        _label.text = $"Drag Cost:${cost}";
+        _label.text = $"${cost}";
 
         _label.style.color = canAfford
-            ? new StyleColor(new Color(1f, 0.7f, 0.7f)) // soft red
-            : new StyleColor(Color.red);
+            ? new StyleColor(Color.white)
+            : new StyleColor(new Color(1f, 0.25f, 0.25f)); // soft red
     }
-    
 
     public void Hide()
     {
