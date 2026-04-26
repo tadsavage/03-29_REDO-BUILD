675a606583cff1b255c0ca76805e96967057c16a
tadsavage <77761466+tadsavage@users.noreply.github.com>
2026-04-26 13:30:33 -0500

    ???

--- Assets/3. UI/BottomHud/BuildMenuUI.cs
+++ Assets/3. UI/BottomHud/BuildMenuUI.cs
@@ -254,6 +254,7 @@ public class BuildMenuUI : MonoBehaviour
     private void LoadGame()
     {
         Debug.Log("LoadGame() START");
+<<<<<<< HEAD
 
         var data = SaveSystem.Load("tad");
         Debug.Log(data == null ? "LoadGame: data is NULL" : "LoadGame: data loaded OK");
@@ -263,6 +264,12 @@ public class BuildMenuUI : MonoBehaviour
         //Debug.Log($"{(data == null ? "LoadGame: data is NULL" : "LoadGame: data loaded OK")}");
         //Debug.Log($"LoadName: {data?.saveName}, money: {data?.money}, placedObjects count: {data?.placedObjects.Count}");
 =======
+>>>>>>> parent of 3c2bc81 (Finally working Save load - good save window - everything working)
+=======
+
+        var data = SaveSystem.Load("tad");
+        Debug.Log(data == null ? "LoadGame: data is NULL" : "LoadGame: data loaded OK");
+
 >>>>>>> parent of 3c2bc81 (Finally working Save load - good save window - everything working)
         if (data == null)
             return;
