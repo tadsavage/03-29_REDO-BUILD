4ae6e605538a6169f44ba07eb6b6583e98e2f8f3
tadsavage <77761466+tadsavage@users.noreply.github.com>
2026-05-20 18:56:53 -0500

    Reapply "workin at work"
    This reverts commit 323a9875db4afdadd15e5a63008ceb01309321db.

--- Assets/1. Scripts/1. FSM/1. Controllers/RaycastController.cs
+++ Assets/1. Scripts/1. FSM/1. Controllers/RaycastController.cs
@@ -8,6 +8,7 @@ public class RaycastController : MonoBehaviour
 
     [SerializeField] private Camera _camera;
     [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private LayerMask _objectMask;
     [SerializeField] private PlacementGrid _grid;
 
     [Header("Debug")]
@@ -97,7 +98,7 @@ private bool _enabled;
         // ---------------------------------------------------------
         // 2. Object raycast (no mask)
         // ---------------------------------------------------------
-        if (Physics.Raycast(ray, out RaycastHit objHit, 500f))
+        if (Physics.Raycast(ray, out RaycastHit objHit, 200f, _objectMask))
             HitObject = objHit.collider.gameObject;
         else
             HitObject = null;
